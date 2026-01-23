907a92156f526f8e5f2e4f34cf59afd70ad9a9d4
EslamKamel89 <elsamkamelforex@gmail.com>
2026-01-23 14:05:10 +0200

    chore(home): add simple hello log for debugging

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,10 @@ import CustomButton from "@/components/CustomButton";
 export default function Home() {
   const secret = process.env.SECRET;
   console.log(secret);
+  const sayHello = () => {
+    console.log("Hello");
+  };
+  sayHello();
   return (
     <div>
       <main>hello from the server</main>
